f26d6d9110dab206f8339a2c8cee5a78ba51412e
amaury-jaspar <amaury.jaspar@etu.umontpellier.fr>
2020-03-03 18:31:32 +0100

    retrieved file names and folders, spaces are causing issues

--- notable_tags_to_folders.sh
+++ notable_tags_to_folders.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-$DIR = $1
+DIR=$1
+DEST=$2
 
-for FILE in DIR; do
-    $PATH = `sed "s/ //g" $FILE | grep tags |cut -d ":" -f 2 |cut -d "[" -f 2 |cut -d "]" -f 1 | cut -d "Notebooks/" -f 2`
-done
+if [ ! -d "$DIR" ]; then
+    echo "No such directory $DIR"
+    exit
+else
+    if [ ! -d "$DEST" ]; then
+        mkdir $DEST
+    fi
+    for FILE in $DIR/*.md; do
+        if [ -e $FILE ]; then
+            NOTEBOOK=`sed "s/ //g" $FILE | grep tags |cut -d ":" -f 2`
+            NOTE_PATH=`echo ${NOTEBOOK#*/} |cut -d "]" -f 1`
+            mkdir -p ${DEST}"/"${NOTE_PATH}
+            # echo ${DEST}"/"${NOTE_PATH}
+            cp $FILE ${DEST}"/"${NOTE_PATH}
+        fi
+    done
+fi
